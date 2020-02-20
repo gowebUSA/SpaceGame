47f9cdbd91f5447eabb2ca2a56fd3d2950411c97
goric <goric@LAPTOP-T3EK1B8H>
2020-02-20 10:25:43 -0800

    Updated TradeGoods class

--- code/SpaceGame/TradeGood/TradeGoods.cs
+++ code/SpaceGame/TradeGood/TradeGoods.cs
@@ -5,5 +5,6 @@ namespace TradeGood
     public class TradeGoods
     {
         //TODO: New Constructor
+        //Richard Go was here. Was working on TradeGoods class but accidentally deleted the Commits that I was working on for the past weeks.
     }
 }
